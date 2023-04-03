9ff6f768036d15141cad87327bbcc55f3f2355b4
Kunal Singh <kunalsin9h@gmail.com>
2023-04-04 00:07:01 +0530

    implemented /health_check and config lld for linker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{guard, App, HttpResponse, HttpServer, Responder, Route};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // HttpServer handles all transport level concerns.
+    HttpServer::new(|| {
+        // App is where all your application logic lives: routing, middlewares, request handlers, etc.
+        App::new().route(
+            "/health_check",
+            Route::new().guard(guard::Get()).to(health_check),
+        )
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
