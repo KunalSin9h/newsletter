d9dee0d3407fd8542874702dc87bc5c0a454f2bd
Kunal Singh <kunalsin9h@gmail.com>
2023-05-27 19:02:12 +0530

    add test for POST /subscribe

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,27 @@
 use actix_web::dev::Server;
-use actix_web::{guard, App, HttpResponse, HttpServer, Route};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+// GET /health_check
+// Health Check is an basic endpoint for check the status of the server
 async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
+    return HttpResponse::Ok().finish();
+}
+
+// POST /subscribe
+// Subscribe to email newsletter
+async fn subscribe() -> HttpResponse {
+    return HttpResponse::Ok().finish();
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-        App::new().route(
-            "/health_check",
-            Route::new().guard(guard::Get()).to(health_check),
-        )
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
 
-    Ok(server)
+    return Ok(server);
 }
