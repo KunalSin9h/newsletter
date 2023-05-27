bea7898e836fccd1886dd7b7bf2dca30afbf1e50
Kunal Singh <kunalsin9h@gmail.com>
2023-05-27 19:02:12 +0530

    add test for POST /subcribe

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,17 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let lst = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind :8000 at main");
-    println!("Started server at post 8000");
-    run(lst)?.await
+    // Get port from environment variable
+    // if not specified then use a random port available
+    let mut port = std::env::var("SERVER_PORT").unwrap_or("0".into());
+
+    // A tcp listener for listening on port
+    let lst = TcpListener::bind(format!("127.0.0.1:{}", port))
+        .expect(&format!("Failed to create TCP Listener at port :{}", port));
+
+    // Get random port if any
+    port = lst.local_addr().unwrap().port().to_string();
+
+    println!("Started server at post {}", port);
+    return run(lst)?.await;
 }
