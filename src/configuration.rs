47bdf0be7e0ce8e518235e420e6fb496ebdc6db2
Kunal Singh <kunalsin9h@gmail.com>
2023-06-09 23:53:26 +0530

    add config

---
+++ src/configuration.rs
@@ -0,0 +1,26 @@
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Initialize our configuration reader
+    let mut settings = config::Config::default();
+
+    // Looking for a top-level file with an extension
+    // that `config` know how to parse: YAML, JSON, etc.
+    settings.merge(config::File::with_name("configuration"))?;
+
+    // convert the configuration values it read into settings type
+    settings.try_into()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use std::net::TcpListener;
 
+use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Get port from environment variable
-    // if not specified then use a random port available
-    let mut port = std::env::var("SERVER_PORT").unwrap_or("6969".into());
+    // Panic if we did't read the configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
 
     // A tcp listener for listening on port
-    let lst = TcpListener::bind(format!("127.0.0.1:{}", port))
-        .unwrap_or_else(|_| panic!("Failed to create TCP Listener at port: {}", port));
+    let lst = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
 
-    // Get random port if any
-    port = lst.local_addr()?.port().to_string();
-
-    println!("Started server at post {}", port);
+    println!("Started server at post {}", configuration.application_port);
     run(lst)?.await
 }
