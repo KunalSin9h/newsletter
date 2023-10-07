ba55bdd1dd924215475fd17af5dce894b17d5252
Kunal Singh <kunalsin9h@gmail.com>
2023-10-07 21:20:46 +0530

    fix: format

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,9 @@
 mod health_check;
+mod subscription_error;
 mod subscriptions;
 mod subscriptions_confirm;
-mod subscription_error;
 
 pub use health_check::*;
+pub use subscription_error::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
-pub use subscription_error::*;
