a2ec0ee4d0dad67925874ef82371cd5a91da1fc8
Kunal Singh <kunalsin9h@gmail.com>
2023-11-04 15:12:18 +0530

    update migration

--- migrations/20230830114102_create_subscription_tokens_table.sql
+++ migrations/20230830114102_create_subscription_tokens_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE subscription_tokens (
     subscription_token TEXT NOT NULL,   
-    subscription_id uuid NOT NULL REFERENCES subscriptions (id),
+    subscription_id uuid NOT NULL REFERENCES subscriptions (id) ON DELETE CASCADE,
     PRIMARY KEY (subscription_token)
 );
